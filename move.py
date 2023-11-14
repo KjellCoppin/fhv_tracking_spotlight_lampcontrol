cc446939e34d2f33b4c86cba5603d02517a3a1a2
KjellCoppin <kjellcoppin@gmail.com>
2023-11-14 10:21:40 +0000

    added python lamp control file

---
+++ move.py
@@ -0,0 +1,19 @@
+import sys
+import asyncio
+from pyartnet import ArtNetNode
+
+async def main():
+	node = ArtNetNode('192.168.0.200', 6454)
+	universe = node.add_universe(0)
+
+	X = (int(sys.argv[1])-1)*19  + 90
+	Y = (int(sys.argv[2])-1)*19
+
+	channel = universe.add_channel(start=1, width=4)
+
+	channel.add_fade([X, Y, 0, 5], 1)
+
+	await channel
+
+
+asyncio.run(main())
