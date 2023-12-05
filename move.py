25d6ddc5bff28a0fb65200a005938f2c1ff90d7e
KjellCoppin <kjellcoppin@gmail.com>
2023-12-05 12:33:37 +0000

    changed scale for move.py

--- move.py
+++ move.py
@@ -6,7 +6,8 @@ async def main():
 	node = ArtNetNode('192.168.0.200', 6454)
 	universe = node.add_universe(0)
 
-	X = (int(sys.argv[1]) * 5) + 1
+	X = round((int(sys.argv[1]) * 2.5))
+	print(X)
 	# Y = (int(sys.argv[2])-1)*19
 
 	channel = universe.add_channel(start=1, width=4)
